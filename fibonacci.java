5d8394f5eceeca39e8e4d74b0d0e7ea7784b5d9a
Puru Pathak <puruppathak@gmail.com>
2016-02-15 14:18:27 -0500

    Calculating Fibonacci output of a given element

---
+++ fibonacci.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+class fib1{
+	
+	public static void main(String args[])
+	{
+		int s;
+		Scanner in = new Scanner(System.in);
+		 
+		System.out.println("Enter the number for calculating its Fibonacci output: \n");
+	      s = in.nextInt();
+		
+		System.out.printf("Fibonacci result for %d is %d",s,calc_fib(s));
+		
+	}
+	
+	public static int calc_fib(int n)
+	{
+		
+		if(n==1)
+			return 0;
+		else if(n==2)
+			return 1;
+		else 
+		{
+			return calc_fib(n-1) + calc_fib(n-2);
+			
+			
+		}
+		
+		
+	}
+	
+}
