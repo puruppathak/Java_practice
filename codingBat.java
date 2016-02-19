fe910101fedd5f8f62ff143deb3bcd6cb84ee109
Puru Pathak <puruppathak@gmail.com>
2016-02-18 19:06:38 -0500

    front22 function created

--- codingBat.java
+++ codingBat.java
@@ -19,4 +19,22 @@ public boolean or35(int n) {
 }
 
 
+public String front22(String str) {
+  
+  if(str.length()<2)
+  return str+str+str;
+  else
+  {
+  
+  String interm = str.substring(0,2);
+  return interm+str+interm; 
+  
+  }
+  
+  
+  
+}
+
+
+
 }
