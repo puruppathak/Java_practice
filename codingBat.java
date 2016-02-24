690db21849a19703c88ff3d09ac5d06b4969370a
Puru Pathak <puruppathak@gmail.com>
2016-02-24 15:01:17 -0500

    Created function startOz

--- codingBat.java
+++ codingBat.java
@@ -129,7 +129,19 @@ public boolean mixStart(String str) {
   
 }
 
-
+public String startOz(String str) {
+  String result = "";
+  
+  if (str.length() >= 1 && str.charAt(0)=='o') {
+    result = result + str.charAt(0);
+  }
+  
+  if (str.length() >= 2 && str.charAt(1)=='z') {
+    result = result + str.charAt(1);
+  }
+  
+  return result;
+}
 
 
 }
