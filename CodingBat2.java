c4282c4a473bfeb3ff895e398b17af18290dda57
Puru Pathak <puruppathak@gmail.com>
2016-02-27 15:19:06 -0500

    Created function StringTimes

--- CodingBat2.java
+++ CodingBat2.java
@@ -8,6 +8,20 @@ public static void main(String[] args) {
 }
 
 
+public String stringTimes(String str, int n) {
+  
+  String result="";
+  int i=0;
+  for(i=0;i<n;i++)
+  {
+  
+  result = result + str;
+  
+  }
+  return result;
+  
+  
+}
 
 
 
